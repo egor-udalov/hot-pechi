e08e30a65467db3dfc6d5a77f59bdf881d7cce31
egor-udalov <zoraudalov@gmail.com>
2024-03-24 14:13:17 +0300

    creat project

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style/reset.css';
+import './style/fonts.css';
+import './style/vars.css';
+import './style/base-styles.css';
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
